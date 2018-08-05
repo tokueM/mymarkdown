3435388abbbfb910ca7244397250c1669ae7a119
mitoku012020 <mitoku012020@gmail.com>
2018-08-05 01:44:39 -0400

    Google Auth add

---
+++ src/components/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="home">
+    <h1>{{ msg }}</h1>
+    <button @click="googleLogin">Googleアカウントでログイン</button>
+  </div>
+</template>
+<script>
+export default {
+  name: 'home',
+  data() {
+    return {
+      msg: "Welcome to Mymarkdown"
+    };
+  },  
+  methods: {
+    googleLogin: function() {
+      firebase.auth().signInWithRedirect(new firebase.auth.GoogleAuthProvider());
+    }
+  }
+};
+</script>
